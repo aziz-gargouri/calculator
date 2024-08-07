4427d413354e907fa1afda1c013a089e92e873ef
aziz-gargouri <azizgargouri67@gmail.com>
2024-08-07 19:03:01 +0100

    calculator 3.0

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("aziz")
+let output=document.getElementById("output")
+
+
+const clicked=(event)=>{
+    
+    output.value+=event.target.value
+}
+
+
+const calculate=()=>{
+    result=eval(output.value)
+    output.value=result
+
+}

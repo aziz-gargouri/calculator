c3d0baf2312e11bb6a58c0f13168f86036658c2f
aziz-gargouri <azizgargouri67@gmail.com>
2024-08-09 16:25:58 +0100

    calculator 90% done

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-let output=document.getElementById("output")
+let output = document.getElementById("output")
 
 
-const clicked=(event)=>{
-    
-    output.value+=event.target.value
+const clicked = (event) => {
+
+    output.value += event.target.value
 }
 
 
-const calculate=()=>{
-    result=eval(output.value)
-    output.value=result
+const calculate = () => {
+    result = eval(output.value)
+    output.value = result
 
 }
+
+const reset = (event) => {
+    if (event.target.value === "AC") { output.value = "" }
+    else if (event.target.value === "DEL") {
+        output.value = output.value.slice(0, output.value.length - 1)
+    }
+}
+
